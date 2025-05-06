79af354361e30d3a5ab8fe0e4db8b58887f75af0
Gabriel Castelo <contatogabrielcastelo1@gmail.com>
2025-05-06 10:23:15 -0300

    Upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "painel-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/manifest.json",
+  "/chamada.mp3",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
+// Instala o Service Worker e adiciona arquivos ao cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativa o Service Worker e remove caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta requisições e responde com o cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
